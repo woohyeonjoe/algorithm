c6eefa01397dcf2692e9764f936b0e8339dfc2e4
woohyeonJoe <106286686+woohyeonjoe@users.noreply.github.com>
2022-09-06 09:35:28 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 32 ms, Memory: 422824 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.java
@@ -0,0 +1,10 @@
+public class Test {
+    long sum(int[] a) {
+        long total = 0;
+        for(int i=0; i<a.length; i++){
+            total += a[i];
+        }
+
+        return total;
+    }
+}
