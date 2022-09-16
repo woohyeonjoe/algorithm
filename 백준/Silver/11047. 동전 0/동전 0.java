f49fb3faf27959a2dd9b7d0132ebc2a260fe0dd2
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2022-09-16 21:31:53 +0900

    [Silver IV] Title: 동전 0, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,24 +1,28 @@
 import java.util.*;
+import java.io.*;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        int[] type = new int[n];
-        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<n; i++){
-            type[i] = scanner.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i=n; i>0; i--){
-            count += k/type[i-1];
-            k = k%type[i-1];
+        int count = 0;
+        for(int i=N-1; i>=0; i--) {
+            if(A[i]<= K) {
+                count += K/A[i];
+                K = K % A[i];
+            }
         }
 
         System.out.println(count);
-
     }
 }
