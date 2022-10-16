b1989c99a8901cdd64005dd655477fb290c3ac58
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2022-10-16 21:39:49 +0900

    [Bronze IV] Title: 기찍 N, Time: 1116 ms, Memory: 33244 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        for(int i=n; i>0; i--){
+            System.out.println(n--);
+        }
+    }
+}
