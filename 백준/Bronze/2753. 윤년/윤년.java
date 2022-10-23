a564c5947897e188aca5e83fbe756bec9e311c47
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2022-10-23 22:52:00 +0900

    [Bronze V] Title: 윤년, Time: 236 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        if(a%4==0 && a%100!=0 || a%400==0){
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
