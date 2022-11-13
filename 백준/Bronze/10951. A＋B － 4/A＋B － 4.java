0922e0173b280b3e8fdc59789d66de62346df67f
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2022-11-13 13:44:51 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            for(;;) {
+                int n1 = scanner.nextInt();
+                int n2 = scanner.nextInt();
+
+                System.out.println(n1+n2);
+            }
+        } catch(Exception e){
+
+        }
+
+
+
+
+    }
+}
