a272f5ebd7122a64ecd932c599b12ee721527485
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2023-02-06 11:17:25 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,22 +1,15 @@
-import java.util.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        
         int length = num_list.length;
+        int[] arr = new int[length];
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        
+        int idx = 0;
         for(int i=length-1; i>=0; i--) {
-            arr.add(num_list[i]);
+            arr[idx] = num_list[i];
+            idx++;
         }
         
-        int[] result = new int[length];
-        for(int i=0; i<length; i++) {
-            result[i] = arr.get(i);
-        }
-        
-        return result;
+        return arr;
     }
 }
