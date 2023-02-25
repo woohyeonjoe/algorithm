4f6d53463004281fc88f6f9405a5e09c349c198a
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2023-02-25 21:41:48 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(a+b+c);
+    }
+
+}
