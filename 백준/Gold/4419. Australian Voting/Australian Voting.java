92aba695daa2f664598bf32eaa24ba58888b1ccf
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2023-04-04 15:53:32 +0900

    [Gold V] Title: Australian Voting, Time: 180 ms, Memory: 16892 KB -BaekjoonHub

--- 백준/Gold/4419. Australian Voting/Australian Voting.java
+++ 백준/Gold/4419. Australian Voting/Australian Voting.java
@@ -1,106 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Main {
+    public static void main(String[] args) throws Exception{
 
-    public static HashMap<String, Integer> candidates = new HashMap<>();
-    public static int n;
-
-    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String[] name = new String[n];
+
+        for(int i=0; i<n; i++){
+            name[i] = br.readLine();
+        }
         String input = "";
-
-        n = Integer.parseInt(br.readLine());
-        ArrayList<String> nameList = new ArrayList<>();
-
-        // 17 ~ 40 initialize
-        for(int i=0; i<n; i++) {
-            String name = br.readLine();
-            candidates.put(name, 0);
-            nameList.add(name);
+        Queue<Integer>[] score = new LinkedList[1001];
+        int count = 0;
+        int[] v = new int[1001];
+        while((input = br.readLine()) != null && input.length() != 0){
+            String[] arr = input.split(" ");
+            score[count] = new LinkedList<>();
+            for(int i=0; i<n; i++){
+                score[count].add(Integer.parseInt(arr[i])-1);
+            }
+            count ++;
         }
-
-        int votes[][] = new int[1000][1000];
-
-        int idx = 0;
-        while((input = br.readLine())!= null){
-            StringTokenizer st = new StringTokenizer(input);
-
-            for(int i=0; i<n; i++)
-                votes[idx][i] = Integer.parseInt(st.nextToken());
-
-            idx ++;
-
-        }
-
-        for(int i=0; i<idx; i++){
-            String name = nameList.get(votes[i][0]-1);
-            candidates.put(name, candidates.getOrDefault(name, 0)+1);
-        }
-
         while(true){
-            // 44 ~ 45 투표순으로 후보자 오름차순 정렬
-            List<Map.Entry<String, Integer>> entryList = new LinkedList<>(candidates.entrySet());
-            entryList.sort(Map.Entry.comparingByValue());
-
-            // 최다 득표수의 백분율
-            double max = (entryList.get(entryList.size()-1).getValue() / (double) idx) * 100;
-
-            int minValue = entryList.get(0).getValue();
-            int maxValue = entryList.get(entryList.size()-1).getValue();
-
-            // 최다 득표수가 과반 수 이거나 최대 값과 최소 값이 같으면 후보자들을 출력하고 종료 54 ~ 69
-            if(max > 50 || minValue == maxValue) {
-                Stack<String> st = new Stack<>();
-
-                // 최대 득표 후보가 여러명 있을 수 있으므로 모두 출력
-                for(int i=entryList.size()-1; i>=0; i--){
-                    st.push(entryList.get(i).getKey());
-
-                    if(i == 0 || entryList.get(i).getValue() != entryList.get(i-1).getValue()) break;
+            for(int i=0; i<count; i++){
+                while(v[score[i].peek()] == -1){
+                    score[i].poll();
                 }
-
-                // entryList를 맨 끝에서 부터 탐색 했기 때문에 거꾸로 다시 출력
-                while(!st.isEmpty())
-                    System.out.println(st.pop());
-
-                break;
+                v[score[i].peek()] ++;
+            }
+            int maxi = 0, mini = 123456789;
+            for(int i=0; i<n; i++){
+                maxi = Math.max(maxi,v[i]);
+                if(v[i] != -1){
+                    mini = Math.min(mini,v[i]);
+                }
             }
 
-            // 최소 득표 후보자들을 제거 (최소 득표 후보자들이 여러명 있을 수 있으므로 모두 제거) (72 ~ 83)
-            for(int i=0; i<entryList.size()-1; i++, idx--){
-                String name = entryList.get(i).getKey();
-                int index = nameList.indexOf(name);
-
-                entryList.remove(0);
-                candidates.remove(name);
-
-                // 최소 득표자 제거 후 다음 순위 후보자에게 표를 넘겨줌
-                findNextVote(votes, index+1, nameList, minValue);
-
-                if(minValue != entryList.get(0).getValue()) break;
+            if(maxi*2 > count || maxi == mini){
+                for(int i=0; i<n; i++){
+                    if(v[i] == maxi){
+                        System.out.println(name[i]);
+                    }
+                }
+                return;
             }
-        }
-
-    }
-
-    public static void findNextVote(int [][] votes, int idx, ArrayList<String> nameList, int minValue){
-
-        for(int i=0; i<votes.length; i++){
-
-            if(votes[i][0] != idx) continue;
-
-            for(int j=1; j<n; j++){
-                String name = nameList.get(votes[i][j] - 1);
-
-                if(candidates.containsKey(name)){
-                    if(minValue < candidates.get(name))
-                        candidates.put(name, candidates.get(name) + 1);
-                    break;
+            for(int i=0; i<n; i++){
+                if(v[i] == mini){
+                    v[i] = -1;
+                }
+                else if(v[i] != -1){
+                    v[i] = 0;
                 }
             }
         }
     }
-
 }
