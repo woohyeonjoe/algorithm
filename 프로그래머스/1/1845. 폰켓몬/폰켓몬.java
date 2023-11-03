68ba0221a2ae210fd59e5c5da09bb3a223806355
조우현 <106286686+woohyeonjoe@users.noreply.github.com>
2023-11-03 16:01:06 +0900

    [level 1] Title: 폰켓몬, Time: 2.87 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,24 +1,17 @@
 import java.util.*;
 import java.io.*;
 
-/*
- 1. n/2 개를 선택가능
- 2. n/2 개 선택 중 종을 가장 다양하게 선택
- 3. 선택해야하는 수(n/2)가 종의 수보다 크면 종의 수만큼 선택 -> 선택해야하는 수가 종의 수보다 크면 무조건 종의 수를 반환하는게 가장 다양한 종을 선택한 것이다.
- 4. 선택해야하는 수(n/2)가 종의 수보다 작다면 n/2를 출력 -> 선택해야하는 수가 종의 수보다 작으면
- */
 class Solution {
     public int solution(int[] nums) {
-        int selectNum = nums.length / 2;        // 선택해야하는 폰켓몬 수
+        int selectNum = nums.length / 2;
         
-        HashSet<Integer> hashSet = new HashSet<>();
-        
-        // 중복된 폰켓몬 종류 제거
-        for (int num : nums) {
+        HashSet hashSet = new HashSet();
+        for(int num : nums) {
             hashSet.add(num);    
         }
         
-        if (selectNum > hashSet.size()) return hashSet.size();
+        if(selectNum > hashSet.size()) return hashSet.size();
         else return selectNum;
+        
     }
 }
